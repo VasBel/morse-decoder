35890e71205219ebb7b25a313676b68779901816
VasBel <vasilii.belyachkov@gmail.com>
2023-02-05 22:39:14 +0300

    add: morse decoder v1.0

--- src/index.js
+++ src/index.js
@@ -37,8 +37,39 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+  let arr = str.match(/.{2}/g);
+  let resultArr = "";
+  let result = '';
+  let c = 4;
+  console.log(arr);
+  for(let i = 0; i < arr.length; i++){
+    
+    if(arr[i] == '10'){
+      resultArr += ".";
+    }else if(arr[i] == '11'){
+      resultArr += "-";
+    }else if(arr[i] == '**' && arr[i+4] == '**'){
+      resultArr += " ";
+    }  
+    if(i == c){
+      if(arr[i] == '11' && arr[i+1] !== undefined || arr[i] == '10' && arr[i+1] !== undefined){
+        c += 5;
+        resultArr += " ";
+      }else if(arr[i] == '**'){
+        c += 5;
+      }
+       
+    } 
+  }
+  for(i = 0; i < resultArr.split(" ").length; i++){
+    if(resultArr.split(" ")[i].length == 0){
+      result += ' ';
+    }else{
+      result += MORSE_TABLE[resultArr.split(" ")[i]];
+    }
+  }
+  return result;
 }
 
 module.exports = {
